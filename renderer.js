b9685212fe765ff621aeec5dfe009a0712abd4e3
pykingcodepro <yourhelper258@gmail.com>
2024-12-17 17:23:08 +0530

    Added Delete Button

--- renderer.js
+++ renderer.js
@@ -5,18 +5,39 @@ const loadTasks = (list) => {
   ulEl.innerHTML = "";
   list.forEach(task => {
     const liEl = document.createElement("li");
-    liEl.innerText = task;
+    liEl.innerText = task.name;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.setAttribute("type", "button");
+    deleteBtn.innerText = "Delete";
+    deleteBtn.addEventListener("click", e => {
+      e.preventDefault();
+      deleteTask(task.id);
+    })
+
+    liEl.appendChild(deleteBtn);
     ulEl.appendChild(liEl);
   });
 }
+
+const deleteTask = id => {
+  let newList = list.filter(task => task.id != id);
+  // console.log(newList);
+  list = newList;
+  loadTasks(newList);
+};
 
 const btn = document.getElementById("addBtn");
 
 btn.addEventListener("click", e => {
   e.preventDefault();
   const taskName = document.getElementById("taskInput").value;
-  // console.log(taskName);
-  list.push(taskName);
+  list.push({
+    id: list.length != 0 ? list[list.length - 1].id + 1 : 0,
+    name: taskName
+  });
+  console.log(list);
   loadTasks(list);
+  document.getElementById("taskInput").value = "";
 })
 loadTasks(list);
